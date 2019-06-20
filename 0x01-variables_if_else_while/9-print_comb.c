efa4493b29a2a3fff548ba978cfbe407bf9c3710
amartinezre05 <almartinezre05@gmail.com>
2019-06-20 23:01:55 +0000

    List with space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
