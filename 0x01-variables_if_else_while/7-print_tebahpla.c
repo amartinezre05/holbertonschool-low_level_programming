4ca34c78369ac2984e22f5f98c42d9ada9fa9a38
amartinezre05 <almartinezre05@gmail.com>
2019-06-20 21:59:25 +0000

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar ('\n');
+return (0);
+}
