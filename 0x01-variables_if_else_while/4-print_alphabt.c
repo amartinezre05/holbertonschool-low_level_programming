e12c71391ac9d51bd48e369d63902d4955ea0e8e
amartinezre05 <almartinezre05@gmail.com>
2019-06-20 21:10:05 +0000

    Alphabet without  e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'e' && i != 'q')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
