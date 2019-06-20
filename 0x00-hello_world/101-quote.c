bf8ab7c1d15bdbea87dc00f7114f52223fa2036e
amartinezre05 <almartinezre05@gmail.com>
2019-06-20 03:31:28 +0000

    C program with function write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
