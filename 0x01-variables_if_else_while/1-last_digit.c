fe59cde0983d3154dc9b7d3620a192cd3fbadd54
amartinezre05 <almartinezre05@gmail.com>
2019-06-20 19:23:47 +0000

    Random number and last digit.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, u);
 }
 else if (u < 6 && u != 0)
 {
-printf("Last digit of %d is %d and is is less than 6 and not 0\n", n, u);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, u);
 }
 else
 {
