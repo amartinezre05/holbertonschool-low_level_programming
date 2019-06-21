53d0c91acb9b94fb7867429b0c37e8d51c98b182
amartinezre05 <almartinezre05@gmail.com>
2019-06-21 03:18:19 +0000

    List of pair numbers

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+int i, l;
+for (i = 48; i <= 57; i++)
+{
+for (l = 48; l <= 57; l++)
+{
+putchar(i);
+putchar(l);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
