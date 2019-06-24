2028ecaa9f657c4b550b0090db04ba14009292e2
amartinezre05 <almartinezre05@gmail.com>
2019-06-24 19:13:46 +0000

    Create a holberton.h

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+#include "holberton.h"
+int _putchar(char c) 
+{
+_putchar(Holberton);
+return (0);
+}
