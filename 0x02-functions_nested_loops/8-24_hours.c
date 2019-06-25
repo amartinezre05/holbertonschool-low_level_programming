9815cb69c9ae1b4c6aa0a5034d7f7230a4bd15e7
amartinezre05 <almartinezre05@gmail.com>
2019-06-25 15:55:58 +0000

    Print all the hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * Print the hours
+ * jack_bauer - Print all the hours of the day.
  *
+ * Return: To void
  */
 void jack_bauer(void)
 {
 int i, j, k, w;
 for (i = 48; i <= 50; i++)
 {
-for (j = 48; j <= 51; j++)
+for (j = 48; j <= 57; j++)
 {
 for (k = 48; k <= 53; k++)
 {
@@ -19,6 +20,7 @@ _putchar(j);
 _putchar(':');
 _putchar(k);
 _putchar(w);
+_putchar('\n');
 }
 }
 }
