b43fdb9cda50220b8d92108568ea6f63e18465c6
amartinezre05 <almartinezre05@gmail.com>
2019-06-25 23:39:41 +0000

    Print the numbers until 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,28 @@ if (n <= 98)
 {
 for (i = n; i <= 98; i++)
 {
-printf("%d, ", i);
 if (i != 98)
 {
-
+printf("%d, ", i);
+}
+else
+{
+printf("%d\n", i);
+}
 }
 }
 else
 {
 for (i = n; i >= 98; i--)
 {
+if (i != 98)
+{
 printf("%d, ", i);
 }
+else
+{
+printf("%d\n", i);
 }
-_putchar('\n');
+}
+}
 }
