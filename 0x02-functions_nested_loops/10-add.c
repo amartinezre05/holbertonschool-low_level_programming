e98f5b27e91b0e00410eee5269bfb83ad9e096bd
amartinezre05 <almartinezre05@gmail.com>
2019-06-25 21:48:22 +0000

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - Print a sum
+ * @c: The first character to be added
+ * @x: The second character to be added
+ *
+ * Return: To the value of 0
+ */
+int add(int c, int x)
+{
+int sum;
+sum = c + x;
+return (sum);
+}
