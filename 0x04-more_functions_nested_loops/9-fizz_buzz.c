3b1fff301f1f6597924ce3a4af125907a435b3af
amartinezre05 <almartinezre05@gmail.com>
2019-06-27 22:56:37 +0000

    Test FizzBuzz.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ if (n % 3 == 0)
 {
 printf("Fizz ");
 }
-else if (n % 5 == 0)
+else if ((n % 5 == 0) && (n <= 99))
 {
 printf("Buzz ");
 }
@@ -22,9 +22,9 @@ else if ((n % 3 == 0) && (n % 5 == 0))
 {
 printf("FizzBuzz ");
 }
-else if (n <= 99)
+else if (n == 100)
 {
-printf("%d, ", n);
+printf("Buzz");
 }
 else
 {
