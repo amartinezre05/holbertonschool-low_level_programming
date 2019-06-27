30eb64445bba309e5437b7114c954a666e6e2cca
amartinezre05 <almartinezre05@gmail.com>
2019-06-27 16:04:54 +0000

    function for multiplicate

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - Return a value of the multiplication of two integers.
+ * @a: The first integer.
+ * @b: The second integer
+ *
+ * Return: To the value of 0.
+ */
+int mul(int a, int b)
+{
+int mult;
+mult = a * b;
+_putchar(mult + '0');
+return (0);
+}
