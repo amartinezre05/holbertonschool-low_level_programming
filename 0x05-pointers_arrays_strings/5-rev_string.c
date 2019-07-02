37cb71f706a42a1efa37f1404d0cdc8562a63c71
amartinezre05 <almartinezre05@gmail.com>
2019-07-02 19:20:29 +0000

    Print numbers multiples of 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,10 +2,23 @@
 
 /**
  * rev_string - Reverse a string.
- * @c: The pointer
+ * @s: The pointer
  *
  * Return: To the value of void.
  */
 void rev_string(char *s)
 {
-
+int i;
+int cont;
+char A[500];
+cont = 0;
+for (i = 0; s[i] != 0; i++)
+{
+}
+cont = cont + i;
+while (cont >= 0)
+{
+A[cont]= s[cont];
+cont--;
+}
+}
