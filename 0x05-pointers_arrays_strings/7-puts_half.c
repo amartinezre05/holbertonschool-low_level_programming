6abf7b7dff5e3c2ab9618f7cfd4abd6fd62835a6
amartinezre05 <almartinezre05@gmail.com>
2019-07-02 19:45:04 +0000

    Print the half of the list

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Print half of a string.
+ * @str: The pointer.
+ *
+ * Return: To the value of void..
+ */
+void puts_half(char *str)
+{
+int i;
+int cont;
+int pr;
+cont = 0;
+for (i = 0; str[i] != 0; i++)
+{
+}
+cont = i;
+if (cont % 2 == 0)
+{
+for (pr = (cont / 2); pr <= cont; pr++)
+{
+_putchar(str[pr]);
+}
+}
+else
+{
+for (pr = ((cont - 1) / 2); pr <= cont; pr++)
+{
+_putchar(str[pr]);
+}
+}
+_putchar('\n');
+}
