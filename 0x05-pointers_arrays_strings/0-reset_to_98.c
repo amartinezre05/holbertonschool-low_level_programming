99598e1aaa95f6ae277a3bc86c3dcb9aa574935a
amartinezre05 <almartinezre05@gmail.com>
2019-07-02 02:09:15 +0000

    Dereferencing to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Function to reset a address with pointers.
+ * @n: The pointer.
+ *
+ * Return: To the value of void.
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
