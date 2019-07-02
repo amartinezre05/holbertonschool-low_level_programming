e6ec31b12cc92d7c9376ec927c6f949029459df5
amartinezre05 <almartinezre05@gmail.com>
2019-07-02 02:35:25 +0000

    Swap the value with dereferencing

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - Function to swap the value of two integers.
+ * @a: The pointer.
+ * @b: The pointer
+ *
+ * Return: To the value of void.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
