8f5671b865eee581d2c23ea5813f52281f79a8e4
amartinezre05 <almartinezre05@gmail.com>
2019-07-03 22:29:32 +0000

    Print the string.

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,15 +10,11 @@
 char *_strcpy(char *dest, char *src)
 {
 int i;
-int j;
 i = 0;
 while (src[i] != 0)
 {
+dest[i] = src[i];
 i++;
-}
-for (j = 0; j <= i + 1; j++)
-{
-dest[j] = src[j];
 }
 return (dest);
 }
