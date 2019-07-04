d89839801e44883d367c76c4abc0caf361925775
amartinezre05 <almartinezre05@gmail.com>
2019-07-04 14:49:16 +0000

    Concatenates strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+ #include "holberton.h"
+
+/**
+ * *_strcat - Concatenates two strings..
+ * @dest: The first pointer.
+ * @src: The second pointer
+ *
+ * Return: To the value of dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[i] != 0)
+{
+dest[i] = src[i];
+i++;
+}
+return (0);
+return (dest);
+}
