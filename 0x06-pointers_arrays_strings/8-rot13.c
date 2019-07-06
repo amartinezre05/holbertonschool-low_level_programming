023ee91179df0cfcd84612e98f2857ae9591d549
amartinezre05 <almartinezre5@gmail.com>
2019-07-06 18:05:35 +0000

    letter substitution cipher

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* rot13 - Changes values of the string.
+* @s: The pointer.
+*
+* Return: To the value of s.
+*/
+char *rot13(char *s)
+{
+int i;
+int j;
+char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rep[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != 0; i++)
+{
+for (j = 0; let[j] != 0; j++)
+{
+if (let[j] == s[i])
+{
+s[i] = rep[j];
+}
+}
+}
+return (s);
+}
+
