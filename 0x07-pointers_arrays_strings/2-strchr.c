844951001c42e1d2133c09ff190a23d5c21dc572
amartinezre05 <almartinezre05@gmail.com>
2019-07-08 18:13:26 +0000

    Return until of string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _strchr - Returns a pointer to the first occurrence of the character.
+* @s: The pointer.
+* @c: Char to replace.
+*
+* Return: To the value of s.
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+break;
+}
+s++;
+}
+return (s);
+}
