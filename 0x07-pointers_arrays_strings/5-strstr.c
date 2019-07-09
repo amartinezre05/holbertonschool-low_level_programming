9a9d78fbce5dac039a563e70a60d5f6740e5b0bd
amartinezre05 <almartinezre5@gmail.com>
2019-07-09 04:03:36 +0000

    Changes the structure

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,23 +12,22 @@ char *_strstr(char *haystack, char *needle)
 {
 int i;
 int j;
-int cont = 0;
+int k = 0;
 for (i = 0; haystack[i] != 0; i++)
 {
 for (j = 0; needle[j] != 0; j++)
 {
 if (haystack[i] == needle[j])
 {
+k = i;
 while (haystack[i] == needle[j])
 {
-cont = cont + 1;
 i++;
 j++;
-printf("%d", i);
 }
 }
 break;
 }
 }
-return (haystack + cont);
+return (haystack + k);
 }
