130d37f307f2bf91eda88199925bb908109a0eaf
amartinezre05 <almartinezre5@gmail.com>
2019-07-09 02:10:02 +0000

    Use pointers to pointers

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* set_string - sets the value of a pointer to a char.
+* @s: The pointer to pointer.
+* @to: The pointer.
+*
+* Return: To the value of void.
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
