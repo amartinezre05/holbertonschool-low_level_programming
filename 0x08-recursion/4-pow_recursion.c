3be933d57b2720aa32001a23a7def57d67b8ba2d
amartinezre05 <almartinezre05@gmail.com>
2019-07-10 21:49:51 +0000

    The raised integers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - returns the value of x raised to the power of y.
+* @x: The power
+* @y: The exponent
+*
+* Return: To the value of int.
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+return (x * _pow_recursion(x, y - 1));
+}
