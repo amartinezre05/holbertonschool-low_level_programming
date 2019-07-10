e85800cbe314c53faa2a95dbd1863addce8e5fdc
amartinezre05 <almartinezre05@gmail.com>
2019-07-10 19:25:26 +0000

    Modificate the return

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,9 @@
 */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-	_strlen_recursion(s + 1);
-	return (a + 1)
+		return (0);
 	}
-return (s = a);
+return (1 + _strlen_recursion(s + 1));
 }
