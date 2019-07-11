02a54126284824ad79d95790fa8d9ba557d56e28
amartinezre05 <almartinezre5@gmail.com>
2019-07-11 03:25:48 +0000

    Create a file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+* _sqrt_recursion - returns the value of x raised to the power of y.
+* @n: The number to evaluate
+*
+* Return: To the value of int.
+*/
+int _sqrt_recursion(int n)
+{
