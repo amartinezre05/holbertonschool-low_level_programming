d279501f8efaf64427f6be8514c1505e27d5c04f
amartinezre05 <almartinezre5@gmail.com>
2019-07-11 22:26:05 +0000

    Added a function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "holberton.h"
 
 /**
+* count - returns the value of y.
+* @y: The number to evaluate
+* @n: The int of the sqrt.
+*
+* Return: To the value of int.
+*/
+int count(int n, int y)
+{
+	if (y * y == n)
+	{
+		return (y);
+	}
+	else if (y * y > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (count(n, y + 1));
+	}
+}
+/**
 * _sqrt_recursion - returns the value of x raised to the power of y.
 * @n: The number to evaluate
 *
@@ -8,3 +30,5 @@
 */
 int _sqrt_recursion(int n)
 {
+	return (count(n, 1));
+}
