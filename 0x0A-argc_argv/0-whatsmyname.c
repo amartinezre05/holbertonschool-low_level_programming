1dba8e9659f219bf4e56bc09b5a906e976234ba3
amartinezre05 <almartinezre5@gmail.com>
2019-07-14 18:07:01 +0000

    Added an value

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function.
+ * @argc: Count unused.
+ * @argv: Array of arguments.
+ *
+ * Return: To the value of 0.
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
