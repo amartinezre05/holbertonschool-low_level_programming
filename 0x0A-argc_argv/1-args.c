b30f76358fdf580a91b96822b8e934dc1c01dc6c
amartinezre05 <almartinezre5@gmail.com>
2019-07-14 18:37:29 +0000

    Change the loop

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - function.
+* @argc: Count.
+* @argv: Array of arguments unused.
+*
+* Return: To the value of 0.
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		++i;
+	}
+	printf("%d\n", i - 1);
+}
