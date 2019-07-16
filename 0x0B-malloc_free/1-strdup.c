f62c2dce551dec43e2e6a0cf67972331e550eab4
amartinezre05 <almartinezre5@gmail.com>
2019-07-16 21:40:31 +0000

    Copy a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _strdup - Copy of the string given as a parameter
+* @str: The string given.
+*
+* Return: The pointer to the array.
+*/
+char *_strdup(char *str)
+{
+int i, j;
+char *b;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	;
+	}
+	b = malloc(sizeof(str) * i);
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	while (j < i)
+	{
+		b[j] = str[j];
+	j++;
+	}
+return (b);
+}
