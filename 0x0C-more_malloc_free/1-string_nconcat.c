8c8356c86ef06792be12c9fba3f91c4d2a73553f
amartinezre05 <almartinezre05@gmail.com>
2019-07-18 18:02:45 +0000

    Concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,8 +3,8 @@
 
 /**
 * string_nconcat - Concatenates two strings.
-* @b: The first string.
-* @b: The second string.
+* @s1: The first string.
+* @s2: The second string.
 * @n: Unsigned int variable, like a limit.
 *
 * Return: To the value of pointer.
@@ -13,6 +13,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j, d, f;
 char *e;
-char *x;
-
-	if (s1
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		;
+	}
+	if (j > n)
+	{
+		j = n;
+	}
+	e = malloc(sizeof(char) * (i + j + 1));
+	if (e == NULL)
+	{
+		return (NULL);
+	}
+	for (d = 0; s1[d] != 0; d++)
+	{
+		e[d] = s1[d];
+	}
+	for (f = 0; f < j; f++)
+	{
+		e[d] = s2[f];
+		d++;
+	}
+return (e);
+}
