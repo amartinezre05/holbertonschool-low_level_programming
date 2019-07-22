8ded14918de3f30b20c9d2fec842c8e97d449b6a
amartinezre05 <almartinezre05@gmail.com>
2019-07-22 21:12:24 +0000

    Changes a print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,24 +16,9 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-	if (d->age == 0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %.6f\n", d->age);
-	}
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
 	}
-	else
-	{
-		printf("Owner: %s\n", d->owner);
-	}
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
