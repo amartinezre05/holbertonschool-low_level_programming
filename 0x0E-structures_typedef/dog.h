109935fa9bdec1553a22f2c5a9d8a3d141cd256e
amartinezre05 <almartinezre05@gmail.com>
2019-07-22 17:11:25 +0000

    Add documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,18 @@
 #define DOG
 
 /**
- * dog - Typedef for struct dog
- */
+* dog - Typedef for struct dog
+*/
+
+/**
+* struct dog - New type.
+* @name: The char variable.
+* @age: The float variable.
+* @owner: The char variable.
+*
+* Description: Variable of type struct dog.
+*/
 typedef struct dog dog;
-/**¬
-* struct dog - New type.¬
-* @name: The char variable.¬
-* @age: The float variable.¬
-* @owner: The char variable.¬
-*¬
-* Description: Variable of type struct dog.¬
-*/¬
 struct dog
 {
 char *name;
