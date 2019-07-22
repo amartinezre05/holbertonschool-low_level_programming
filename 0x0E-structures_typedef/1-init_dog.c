2a9798a5ad4300ff777511ac3ae5bcb93a20f695
amartinezre05 <almartinezre05@gmail.com>
2019-07-22 20:20:37 +0000

    Add the library

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
 * init_dog - initialize a variable of type struct dog.
@@ -11,4 +12,11 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	if (d == NULL)
+	{
+		return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
