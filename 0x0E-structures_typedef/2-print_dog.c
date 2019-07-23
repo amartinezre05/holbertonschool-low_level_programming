53059823a04bb2ff761658813cff0928ed2d0560
amartinezre05 <almartinezre5@gmail.com>
2019-07-23 01:13:28 +0000

    Changes the float

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,5 +20,5 @@ void print_dog(struct dog *d)
 	{
 		printf("Owner: (nil)\n");
 	}
-	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
