050da327441990006507765f4e15dd6497b56879
amartinezre05 <almartinezre5@gmail.com>
2019-07-23 18:14:53 +0000

    Changes syntaxis

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,21 +22,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (a = 0; name[a] != '\0'; a++)
 		;
+	a++;
 	new->name = malloc(a * sizeof(char));
 	if (new->name == NULL)
+	{
 		free(new);
 		return (NULL);
+	}
 	for (b = 0; b < a; b++)
 		new->name[b] = name[b];
+	new->age = age;
 	for (c = 0; owner[c] != '\0'; c++)
 		;
+	c++;
 	new->owner = malloc(c * sizeof(char));
 	if (new->owner == NULL)
+	{
 		free(new->name);
 		free(new);
 		return (NULL);
+	}
 	for (d = 0; d < c; d++)
 		new->owner[d] = owner[d];
-	new->age = age;
 return (new);
 }
