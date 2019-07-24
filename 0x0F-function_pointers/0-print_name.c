48a398cad7f01a91f18b75c1e035a09cf586ccc3
amartinezre05 <almartinezre05@gmail.com>
2019-07-24 17:43:32 +0000

    Added the condition if the variables are null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 * print_name - a function that prints a name.
@@ -9,8 +11,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
-	{
+	if (name == NULL && f == NULL)
+	return;
 	(*f)(name);
-	}
 }
