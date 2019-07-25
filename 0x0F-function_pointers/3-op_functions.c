7f2a59b3b0d6200cdec2cb6e3e75d364b9db4774
amartinezre05 <almartinezre5@gmail.com>
2019-07-25 02:09:21 +0000

    Explication of all functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* op_add - a function that sum two integers.
+* @a: Variable the type int.
+* @b: Variable the type int.
+*
+* Return: To the value of sum.
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - a function that substract two integers.
+* @a: Variable the type int.
+* @b: Variable the type int.
+*
+* Return: To the value of sub.
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - a function that multiplicate two integers.
+* @a: Variable the type int.
+* @b: Variable the type int.
+*
+* Return: To the value of mul.
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - a function that divided two integers.
+* @a: Variable the type int.
+* @b: Variable the type int.
+*
+* Return: To the value of div.
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - a function that remainder of the division of two integers.
+* @a: Variable the type int.
+* @b: Variable the type int.
+*
+* Return: To the value of mod.
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
