b464c6a749d4136a4e4d587a3e130c7780483567
amartinezre05 <almartinezre05@gmail.com>
2019-07-26 16:48:13 +0000

    Function to print numbers, changes the syntaxis

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_numbers - a function that prints numbers.
+* @n: Constant variable type int.
+* @separator: Constant variable type char.
+*
+* Return: Value of type void.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list list;
+unsigned int i;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator != NULL && i < n - 1)
+		{
+		printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
