3a5158d50b7b21e7a75e787f054d5bff6c1bd218
amartinezre05 <almartinezre05@gmail.com>
2019-08-02 00:14:06 +0000

    Print before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* startupfun - Function to execueted before the main.
+* 
+* Return: Void.
+*
+*/
+void startupfun (void) __attribute__ ((constructor))
+{
+char *str = "I bore my house upon my back!"
+}
+/**
+* startupfun - Function to execueted before the main.
+* 
+* Return: Void.
+*
+*/
+void startupfun (void)
+{
+printf ("You're beat! and yet, you must allow,\n %s", str);
+}
