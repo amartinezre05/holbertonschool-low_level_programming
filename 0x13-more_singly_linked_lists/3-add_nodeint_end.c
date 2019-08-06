401cae4c724c717c785da513af84e60a63e4f1d4
amartinezre05 <almartinezre05@gmail.com>
2019-08-06 16:53:36 +0000

    Added descritpion

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
-* add_nodeint - adds a new node at the end of a listint_t list.
+* add_nodeint_end - adds a new node at the end of a listint_t list.
 * @head: A constant variable of type list_t.
 * @n: Variable type n.
 *
