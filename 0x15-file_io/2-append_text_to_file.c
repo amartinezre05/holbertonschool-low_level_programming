dd3a0f73fc01df9b31fa4235bd6644d5d473b08a
amartinezre05 <almartinezre05@gmail.com>
2019-08-14 03:49:45 +0000

    create a function

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+* _strlen - Returns the length of a string.
+* @s: The pointer.
+*
+* Return: To the value of s.
+*/
+int _strlen(char *s)
+{
+int cont;
+int i;
+i = 0;
+cont = 0;
+while (s[i] != 0)
+{
+i++;
+}
+cont = cont + i;
+return (cont);
+}
+/**
+* append_text_to_file - function that append a text at the end.
+* @filename: The file.
+* @text_content: String.
+*
+* Return: An int.
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, wt;
+
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+{
+close(fd);
+return (-1);
+}
+if (text_content == NULL)
+{
+close(fd);
+return (1);
+}
+wt = write(fd, text_content, _strlen(text_content));
+if (wt == -1)
+{
+close(fd);
+return (-1);
+}
+close(fd);
+return (1);
+}
