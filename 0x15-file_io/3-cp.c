93f0257102be696715a76dcf904001316b22f617
amartinezre05 <almartinezre05@gmail.com>
2019-08-16 19:09:04 +0000

    Modificate the conditionals

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,33 +26,23 @@ void error97(void)
 /**
 * error98 - function that validate
 * @str: char pointer
-* @fd: int
+*
 * Return: void
 */
-void error98(char *str, int fd)
+void error98(char *str)
 {
-	int cl;
-
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
-	cl = close(fd);
-	if (cl == -1)
-		close100(cl);
 	exit(98);
 }
 /**
 * error99 - function that validate
 * @str: char pointer
-* @fd: int
+*
 * Return: void
 */
-void error99(char *str, int fd)
+void error99(char *str)
 {
-	int cl;
-
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
-	cl = close(fd);
-	if (cl == -1)
-		close100(cl);
 	exit(99);
 }
 
@@ -73,11 +63,19 @@ int main(int argc, char **argv)
 	fd1 = open(argv[1], O_RDONLY);
 	rd = read(fd1, buff, 1024);
 	if (rd == -1 || fd1 == -1)
-		error98(argv[1], fd1);
+	{	cl1 = close(fd1);
+		if (cl1 == -1)
+			close100(cl1);
+	error98(argv[1]);
+	}
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	wr = write(fd2, buff, rd);
 	if (fd2 == -1 || wr == -1)
-		error99(argv[2], fd2);
+	{	cl2 = close(fd2);
+		if (cl2 == -1)
+			close100(cl2);
+	error99(argv[2]);
+	}
 	while (rd == 1024)
 	{
 		rd = read(fd2, buff, 1024);
@@ -86,14 +84,8 @@ int main(int argc, char **argv)
 	cl1 = close(fd1);
 	cl2 = close(fd2);
 	if (cl1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cl1);
-		exit(100);
-	}
+		close100(cl1);
 	if (cl2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", cl2);
-		exit(100);
-	}
-	return (0);
+		close100(cl2);
+return (0);
 }
