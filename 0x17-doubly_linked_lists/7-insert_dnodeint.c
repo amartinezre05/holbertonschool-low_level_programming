2068e861c308f5fa99f86d7e0ba0bc6c6e22bdfb
amartinezre05 <almartinezre05@gmail.com>
2019-09-17 00:47:41 +0000

    Fix all the syntaxis

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,18 +8,36 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new;
+dlistint_t *new, *tmp;
 unsigned int i = 0;
+tmp = *h;
 
+	if (idx == 0)
+	{
+		return(add_dnodeint(h, n));
+	}
+	while (i < idx - 1)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+		{
+			return (NULL);
+		i++;
+		}
+	}
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->n = n;
-	while (*h != NULL)
-	{
-		if (i == idx)
-		{
-			new->next = (*h);
-			new->prev = 
+	new->prev = current;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+	return (new)
+}
