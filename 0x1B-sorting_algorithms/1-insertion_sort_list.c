7694ae0382584573c4a9e02dfbf4b12309084cd3
amartinezre05 <almartinezre5@gmail.com>
2019-10-31 01:22:31 +0000

    Added the bigO file and changes the swap function

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,71 +7,42 @@
 */
 void swap(listint_t *nodeL, listint_t *nodeR)
 {
-nodeL->next = nodeR->next;
+listint_t *conL, *conR;
+
+if (nodeL->prev)
+	nodeL->prev->next = nodeR;
+if (nodeR->next)
+	nodeR->next->prev = nodeL;
+conL = nodeL->prev;
+conR = nodeR->next;
+nodeR->prev = conL;
 nodeR->next = nodeL;
-nodeR->prev = nodeL->prev;
 nodeL->prev = nodeR;
-/*	if (nodeL->next)
-	{
-		(nodeL->next)->prev = nodeL;
-	}
-	else
-	{
-		nodeL->next = NULL;
-	}
-	if (nodeR->prev)
-	{
-		(nodeR->prev)->next = nodeR;
-	}
-	else
-	{
-		nodeR->prev = NULL;
-	}*/
-if (nodeR->prev != NULL)
-	(nodeR->prev)->next = nodeR;
-if (nodeL->next != NULL)
-	(nodeL->next)->prev = nodeL;
+nodeL->next = conR;
 }
 /**
-* insertion_sort_list - sort an array using the bubble method
-* @list: int tmp
+* insertion_sort_list - sort an array
+* @list: The double pointer to the head
 * Return: void
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *tmp1, *tmp2;
+listint_t *tmp1;
 
 	if (!list)
-	return;
+		return;
 
 	tmp1 = (*list)->next;
 
 	while (tmp1)
 	{
-	tmp2 = tmp2->next;
-/*	tmp2 = tmp2->next;*/
-		while (tmp2->n < (tmp2->prev)->n)
+		while (tmp1->prev && (tmp1->n < (tmp1->prev)->n))
 		{
-			swap(tmp2->prev, tmp2);
-			if (tmp2->prev == NULL)
-				*list = tmp2;
+			swap(tmp1->prev, tmp1);
+			if (tmp1->prev == NULL)
+				*list = tmp1;
 			print_list(*list);	
 		}
-/*	tmp2 = tmp1;
-	while (tmp2->prev)
-	{
-		if (tmp2->n < (tmp2->prev)->n)
-		{
-			swap(tmp2->prev, tmp2);
-			if (tmp2->prev == NULL)
-			{
-				*list = tmp2;
-			}
-			print_list(*list);
-		}
-	if (tmp2->prev != NULL)
-		tmp2 = tmp2->prev;
-	}*/
 	tmp1 = tmp1->next;
 	}
 }
