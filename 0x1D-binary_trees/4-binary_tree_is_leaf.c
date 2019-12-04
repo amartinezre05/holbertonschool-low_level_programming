4e1d53b8349b77a26d6418dfab50a932c0fa6e66
amartinezre05 <almartinezre05@gmail.com>
2019-12-04 03:24:15 +0000

    [add] check if is a leaf

---
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - deletes a binary tree node
+* @node: a pointer to the parent node of the node to check
+* Return: void
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
