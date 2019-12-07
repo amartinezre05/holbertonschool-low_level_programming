4e3dc14e63ac3c2a1b8ad597f5a9ce6ec9908304
amartinezre05 <almartinezre05@gmail.com>
2019-12-07 00:16:19 +0000

    [fix] special case

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -33,8 +33,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (1);
-	else
-		return (0);
+	{
+		if (!tree->left && !tree->right)
+			return (1);
+		if (tree->left && tree->right)
+			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	}
 return (0);
 }
