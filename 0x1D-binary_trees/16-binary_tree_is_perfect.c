084f50630edcc3aa2c4c9faecc458c98f21abb57
amartinezre05 <almartinezre05@gmail.com>
2019-12-07 00:03:50 +0000

    [add] perfect

---
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+/**
+* binary_tree_height - measures the height of a binary tree
+* @tree: a pointer to the root node of the tree to traverse
+*
+* Return: size_t
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t sum_left = 0, sum_right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		sum_left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		sum_right = 1 + binary_tree_height(tree->right);
+	if (sum_left >= sum_right)
+		return (sum_left);
+	else
+		return (sum_right);
+}
+/**
+* binary_tree_is_perfect - checks if a binary tree is perfect
+* @tree: a pointer to the root node of the tree to check
+*
+* Return: int
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+		return (1);
+	else
+		return (0);
+return (0);
+}
