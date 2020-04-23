2b37d244640f3ffbdc606eca1431859dc8a1020f
amartinezre05 <almartinezre05@gmail.com>
2020-04-22 21:57:38 -0500

    fix betty

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,7 @@
 #ifndef SEARCH_ALGO
 #define SEARCH_ALGO
-
 #include <stdio.h>
 #include <stdlib.h>
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
