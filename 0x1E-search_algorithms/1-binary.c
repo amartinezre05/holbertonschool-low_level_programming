f8077ef28ae97e0f078a2f86f7912c40ad949aa1
amartinezre05 <almartinezre05@gmail.com>
2020-04-22 22:07:23 -0500

    O notation

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search for a value
+ * @array: pointer
+ * @size: number of elements
+ * @value: the number to search
+ * Return: int
+*/
+int binary_search(int *array, size_t size, int value)
+{
+}
