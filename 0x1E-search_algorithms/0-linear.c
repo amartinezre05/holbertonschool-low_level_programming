eedeefa75666b27198107868b561dcb4d9a5d800
amartinezre05 <almartinezre05@gmail.com>
2020-04-22 21:38:12 -0500

    search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for a value
+ * @array: pointer
+ * @size: number of elements
+ * @value: the number to search
+ * Return: int
+*/
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (!array)
+        return (-1);
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+    return (-1);
+}
