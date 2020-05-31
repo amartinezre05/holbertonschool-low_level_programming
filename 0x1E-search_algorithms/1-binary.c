78506bf3892b4798e1c589890294107cacba9950
amartinezre05 <almartinezre05@gmail.com>
2020-05-31 13:11:19 -0500

    search for a value

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,23 @@
 #include "search_algos.h"
 
 /**
+ * print - print values
+ * @array: pointer
+ * @start: start
+ * @end: end
+ * Return: void
+*/
+void print(int *array, size_t start, size_t end)
+{
+	unsigned int i;
+
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
  * binary_search - search for a value
  * @array: pointer
  * @size: number of elements
@@ -9,4 +26,21 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t start, end;
+
+	start = 0;
+	end = size - 1;
+	if (!array)
+		return (-1);
+	while (start <= end)
+	{
+		print(array, start, end);
+		if (array[(start + end) / 2] == value)
+			return ((start + end) / 2);
+		if (array[(start + end) / 2] < value)
+			start = ((start + end) / 2) + 1;
+		else
+			end = ((start + end) / 2) - 1;
+	}
+return (-1);
 }
